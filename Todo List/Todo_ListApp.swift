546a4b9cc9b186215474817dd1a467498acc8562
Roshan Chaudhari <61083597+theroshanchaudhari@users.noreply.github.com>
2023-05-07 22:29:56 -0400

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Roshan Chaudhari on 2023-05-07.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
